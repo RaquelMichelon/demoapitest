8119990a724c519de407b61e0a5b0879d37f6caa
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-25 09:08:06 +0100

    learning how to use mockito to test service layer

--- src/test/java/com/michelon/demoapitest/user/UserServiceTest.java
+++ src/test/java/com/michelon/demoapitest/user/UserServiceTest.java
@@ -4,25 +4,51 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 /**
  * @autor raqueldarellimichelon
  * created on 24/05/23 inside the package - com.michelon.demoapitest.user
+ *
+ * Since userRepository was already tested, we should mock userRepository here.
+ * This way we isolate the service layer to test it.
  **/
+@ExtendWith(MockitoExtension.class) //with this annotation, the Autoclosable is dispensable.
 class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+//    private AutoCloseable autoCloseable;
+    private UserService underTest;
 
     @BeforeEach
     void setUp() {
+        //to initialize the mock; if you have more than one, it will initialize all of them
+//        autoCloseable = MockitoAnnotations.openMocks(this);
+
+        underTest = new UserService(userRepository);
     }
 
-    @AfterEach
-    void tearDown() {
-    }
+//    @AfterEach
+//    void tearDown() {
+//        //to close the resource after each test
+//        autoCloseable.close();
+//    }
 
     @Test
-    void getAllUsers() {
+    void canGetAllUsers() throws Exception {
+        //when
+        underTest.getAllUsers();
+
+        //then - check if repository was invoked using findAll()
+        verify(userRepository).findAll();
+
     }
 
     @Test
